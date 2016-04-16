92acf4c5c56fae125551d44fe479dcba69cc8626
Martin Hochel <hochelmartin@gmail.com>
2016-04-16 21:54:03 +0200

    test(app): add coverage for app.component

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,30 @@
+import { AppModule } from './app';
+import { IRender, renderFactory } from 'ng-metadata/testing';
+import { AppComponent } from './app.component';
+
+describe( `AppComponent`, () => {
+
+  let render: IRender;
+
+  beforeEach( angular.mock.module(AppModule.name) );
+
+  beforeEach( angular.mock.inject( ( $injector: ng.auto.IInjectorService ) => {
+
+    const $compile = $injector.get<ng.ICompileService>( '$compile' );
+    const $rootScope = $injector.get<ng.IRootScopeService>( '$rootScope' );
+    const $scope = $rootScope.$new();
+
+    render = renderFactory( $compile, $scope );
+
+  } ) );
+
+  it( `should render Hello Pluto!!!`, () => {
+
+    const { ctrl, compiledElement } = render( AppComponent );
+
+    expect( ctrl instanceof AppComponent ).toBe( true );
+    expect( compiledElement.text() ).toContain( 'Hello from Pluto!!!' );
+
+  } );
+
+} );
