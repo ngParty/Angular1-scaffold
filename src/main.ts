5f0ee99800d9cbfee114065e6ceb32f2f3c3292f
Martin Hochel <hochelmartin@gmail.com>
2016-05-22 11:42:52 +0200

    feat(app): add enableProdMode on production (#30)
    * feat(app): add enableProdMode no production
    * refactor(webpack): stringify ENV within metadata
    * refactor(main): switch if condition
    * refactor(index): remove unneeded conditional for adding webpack-dev-server
    
    Closes #29

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { bootstrap } from 'ng-metadata/platform';
+import { enableProdMode } from 'ng-metadata/core';
+
 import { AppModule } from './app';
+
+if ( ENV === 'production' ) {
+  enableProdMode();
+}
 
 bootstrap( AppModule );
