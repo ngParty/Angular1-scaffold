34c9a6fee16593dd771ec1c0cb27986212d777d8
Martin Hochel <hochelmartin@gmail.com>
2016-11-09 23:14:11 +0100

    fix(globals): add manual typings so deprecated provide with angular.module doesn't throw TS errors

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,3 +1,6 @@
+// @FIXME remove this line when https://github.com/ngParty/ng-metadata/issues/175 is resolved
+/// <reference path="../node_modules/ng-metadata/manual_typings/globals.d.ts" />
+
 // Extra variables that live on Global that will be replaced by webpack DefinePlugin
 declare var ENV: string;
 interface GlobalEnvironment {
