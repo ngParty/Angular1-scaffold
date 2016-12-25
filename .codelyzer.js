a5bb575f05328b5562e715a7eec6a479247acfb9
Martin Hochel <hochelmartin@gmail.com>
2016-12-25 23:32:36 +0100

    feat(codelyzer): add code transform for ngRepeat

--- .codelyzer.js
+++ .codelyzer.js
@@ -1,3 +1,10 @@
+function transformNg1TemplateToNg2(code) {
+  return code
+    // remove $ctrl prefixes
+    .replace(/\$ctrl\./g, '')
+    // handle ng-repeat micro syntax
+    .replace(/ng-repeat="(\w+)\s+in\s+(\w+)"/, '*ngFor="let $1 of $2"')
+}
 module.exports = {
   // Transformation of the templates. This hooks is quite useful
   // if you're using any other templating language, for instance
@@ -6,6 +13,6 @@ module.exports = {
   // NOTE that this method WILL NOT throw an error in case of invalid template.
   //
   transformTemplate(code, url, decorator) {
-    return {code: code.replace(/\$ctrl\./g, ''), url, decorator};
+    return {code: transformNg1TemplateToNg2(code), url, decorator};
   },
 }
